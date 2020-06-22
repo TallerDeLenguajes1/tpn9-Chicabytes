c8a72777f9b4f6bdf7e0169492e87159582391cf
Chicabytes <chocobarjulietain@gmail.com>
2020-06-21 21:27:12 -0300

    Punto 1

---
+++ ConsoleApp1/ConsoleApp1/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Helpers
+{
+    static class SoporteParaConfiguracion
+    {   
+        public static void CrearArchivoDeConfiguracion(string Direccion)
+        {
+            BinaryWriter BinArch = new BinaryWriter(File.Open("destino.dat", FileMode.Create));
+            BinArch.Write(Direccion);
+            BinArch.Close();
+        }
+        public static string LeerConfiguracion()
+        {
+            BinaryReader BinArchR = new BinaryReader(File.Open("destino.dat", FileMode.Open));
+            return BinArchR.ReadString();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Helpers;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Conf_Leida;
+            string DireccionArch = @"C:\Users\giuli\Documents\Facultad\Taller1\TP9\"; 
+            if (File.Exists("destino.dat"))
+            {
+                Conf_Leida = SoporteParaConfiguracion.LeerConfiguracion();
+            }
+            else
+            {
+                SoporteParaConfiguracion.CrearArchivoDeConfiguracion(DireccionArch);
+                Conf_Leida = SoporteParaConfiguracion.LeerConfiguracion();
+            }
+            MoverArch(Conf_Leida);
+        }
+
+        public static void MoverArch(string Config)
+        {
+            Directory.CreateDirectory(Config);
+            string Directorios = Directory.GetCurrentDirectory();
+            string[] ArreArchDirectory = Directory.GetFiles(Directorios);
+            foreach (string Archivo in ArreArchDirectory)
+            {
+                if (Archivo.Contains(".mp3") || Archivo.Contains(".txt"))
+                {
+                    string[] Cambio = Archivo.Split(@"\");
+                    int Longitud = Cambio.Length;
+                    Directory.Move(Archivo, Config +@"\"+ Cambio[Longitud-1]);
+                }
+            }
+        }
+    }
+}
